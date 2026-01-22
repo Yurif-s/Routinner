6acb36e79f9e51dceaa4d78b43d56044db2e1c27
Yuri <yurimfs30@gmail.com>
2026-01-22 17:17:44 -0300

    feat: adding endpoint to register an user

---
+++ src/Backend/Routinner.Api/Controllers/UserController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Routinner.Api.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class UserController : ControllerBase
+{
+    [HttpPost]
+    public async Task<IActionResult> Register()
+    {
+        return Ok();
+    }
+}
