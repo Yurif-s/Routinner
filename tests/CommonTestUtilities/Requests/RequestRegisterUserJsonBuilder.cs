d87343c34c5fc074b28aba52f4e8e57c6a574f66
Yuri <yurimfs30@gmail.com>
2026-01-24 17:02:24 -0300

    feat: adding request register user builder

---
+++ tests/CommonTestUtilities/Requests/RequestRegisterUserJsonBuilder.cs
@@ -0,0 +1,15 @@
+﻿using Bogus;
+using Routinner.Communication.Requests;
+
+namespace CommonTestUtilities.Requests;
+
+public static class RequestRegisterUserJsonBuilder
+{
+    public static RequestRegisterUserJson Build(int passwordLenght)
+    {
+        return new Faker<RequestRegisterUserJson>()
+            .RuleFor(user => user.Name, f => f.Person.FirstName)
+            .RuleFor(u => u.Name, (f, user) => f.Internet.Email(user.Name))
+            .RuleFor(u => u.Password, f => f.Internet.Password(passwordLenght))
+    }
+}
