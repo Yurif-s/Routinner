5c37e4cf541f054be4cf8e1857ab6f96f7ede581
Yuri <yurimfs30@gmail.com>
2026-01-24 13:54:13 -0300

    feat: adding exceptions and exceptions messages to register user

---
+++ src/Shared/Routinner.Exception/ExceptionsBase/ErrorOnValidationException.cs
@@ -0,0 +1,16 @@
+﻿using System.Net;
+
+namespace Routinner.Exception.ExceptionsBase;
+
+public class ErrorOnValidationException : RoutinnerException
+{
+    private readonly IList<string> _errors;
+    public ErrorOnValidationException(IList<string> errors) : base(string.Empty)
+    {
+        _errors = errors;   
+    }
+
+    public override int StatusCode => (int)HttpStatusCode.BadRequest;
+
+    public override IList<string> GetErrors() => _errors;
+}
---
+++ src/Shared/Routinner.Exception/ExceptionsBase/RoutinnerException.cs
@@ -0,0 +1,10 @@
+﻿namespace Routinner.Exception.ExceptionsBase;
+
+public abstract class RoutinnerException : SystemException
+{
+    public RoutinnerException(string message) : base(message) 
+    { }
+
+    public abstract int StatusCode { get; }
+    public abstract IList<string> GetErrors();
+}
