07a88abfaac2a08361dea49595e3d6181e335842
Yuri <yurimfs30@gmail.com>
2026-02-10 14:28:41 -0300

    feat: adding routine table

---
+++ src/Backend/Routinner.Domain/Entities/Routine.cs
@@ -0,0 +1,9 @@
+﻿namespace Routinner.Domain.Entities;
+
+public class Routine : EntityBase
+{
+    public string Name { get; set; } = string.Empty;
+    public DateOnly StartDate { get; set; }
+    public DateOnly EndDate { get; set; }
+    public long UserId { get; set; }
+}
---
+++ src/Backend/Routinner.Infrastructure/Migrations/Versions/Version02.cs
@@ -0,0 +1,15 @@
+﻿using FluentMigrator;
+
+namespace Routinner.Infrastructure.Migrations.Versions;
+
+[Migration(DatabaseVersions.TABLE_ROUTINE, "Create table to create routine")]
+public class Version02 : VersionsBase
+{
+    public override void Up()
+    {
+        CreateTable("Routine")
+           .WithColumn("Name").AsString(255).NotNullable()
+           .WithColumn("StartDate").AsDate().NotNullable()
+           .WithColumn("EndDate").AsDate().NotNullable();
+    }
+}
